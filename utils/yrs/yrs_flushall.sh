b29fab1a00d1fd2a6add499d040f44694a3a084d
robert.kickhofel <robert.kickhofel@yellowbrick.com>
2026-09-22 11:09:06 -0600

    utils/yrs: yrs_flushall.sh -add no_report and no_trim options.

--- utils/yrs/yrs_flushall.sh
+++ utils/yrs/yrs_flushall.sh
@@ -1,7 +1,7 @@
 # user_rowstore_flush.sh
 #
 # Flush user rowstore tables collecting rowstore stats before and after.
-# 
+#
 #
 # Prerequisites:
 # . Must be run as a superuser
@@ -10,10 +10,13 @@
 #      YBHOST=yb70i1 ./user_rowstore_flush_all.sh
 #
 # Revision History:
-# 2026.04.06 (rek) - Add --report_only option.
+# 2026.09.22 (rek) - . Add -R|--no_report and -t|--trim options
+#                      NOTE: default is now to not automatically trim unused yrs files after the flush.
+#                    . validate yrs_flush_tables.sh found and executable
+# 2026.04.06 (rek) - Add -r | --report_only option.
 # 2026.04.02 (rek) - Add handling for CN where yflush needs to run on the system cluster.
 # 2026.03.13 (rek) - Updates for when running from apliance manager node.
-#                    Output dir changed to ../output/script_name_"$( date +"%Y%m%d_%H%M" ) 
+#                    Output dir changed to ../output/script_name_"$( date +"%Y%m%d_%H%M" )
 # 2026.03.09 (EM)  - Error checks and notification for ybsql
 # 2026.02.05 (rek) - Add yb_yrs_delete_unused_files after flush.
 # 2026.02.05 (rek) - Major refactoring to support
@@ -24,9 +27,8 @@
 # 2024.09.03 (rek) - Initial version.
 #
 # TODO:
-# . add option to only run queries vs queries+flush.
 # . Refactor for global vars set by common scripts
-# . Add README|usage()
+#
 
 ###############################################################################
 # EXTERNAL COMMON FUNCTIONS
@@ -37,7 +39,7 @@ source ../common/connection_fns.sh
 # READONLY VARIABLES
 ###############################################################################
 
-readonly script_version='2026.04.06.1215'
+readonly script_version='2026.09.22.1100'
 readonly script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 readonly script_file_name="$(echo $(basename $0))"
 readonly script_name="$(echo $(basename $0) | cut -f1 -d'.' )"
@@ -125,16 +127,20 @@ function usage()
 # Help/usage text
 #------------------------------------------------------------------------------
 {
-    echo "${script_file_name}:" 
-    echo ". Report on or flush the Yellowbrick RowStore (yrs, a.k.a. the 'user rowstore')."    
+    echo "${script_file_name}:"
+    echo ". Report on or flush the Yellowbrick RowStore (yrs, a.k.a. the 'user rowstore')."
     echo ""
-    echo "Usage:  ${script_file_name} [-?|r]"  
-    echo "   [ -r | --report_only    ] Only display the YRS reports. Does not flush the YRS."     
+    echo "Usage:  ${script_file_name} [-?|r|R|t]"
+    echo "   [ -r | --report_only    ] Only display the YRS reports. Does not flush the YRS."
+    echo "   [ -R | --no_report      ] Do not display the YRS reports after the flush."
+    echo "   [ -t | --trim           ] Trim (delete) unused YRS files after the flush."
     echo "   [ ?  | --help | --usage ] display this help message and exit"
-    echo ""    
-    echo "Examples:"    
-    echo "   ${script_file_name}"    
+    echo ""
+    echo "Examples:"
+    echo "   ${script_file_name}"
     echo "   ${script_file_name} --report_only"
+    echo "   ${script_file_name} --trim"
+    echo "   ${script_file_name} --no_report --trim"
     echo "   YBHOST=yb001 YBUSER=yellowbrick YBPASSWORD='yellowbrick' ${script_file_name} -r"
     echo ""
     echo "Prerequisites:"
@@ -150,17 +156,23 @@ function usage()
 
 function get_opts()
 #------------------------------------------------------------------------------
-# Process command line arguments  
+# Process command line arguments
 # $1 - The args array. i.e. "$@"
 #------------------------------------------------------------------------------
 {
 	while [[ $# > 0 ]]
 	do
 		opt="$1"
-		case $opt in                                         
+		case $opt in
 			-r|--report_only)
-				opt_report_only='t'  
-				;;                                   
+				opt_report_only='t'
+				;;
+			-R|--no_report)
+				opt_no_report='t'
+				;;
+			-t|--trim)
+				opt_trim='t'
+				;;
 			-?|--help|--usage)
 				usage
 				exit 0;
@@ -208,62 +220,70 @@ function main()
   local is_cn="f"
   local default_cluster="none"
   local system_cluster="none"
-  
+
   print_section_hdr "script"
   print_property "script_file_name" "${script_file_name}"
   print_property "start_time" $( date +"%Y.%m.%d_%H%M" )
   print_property "${script_file_name} version" "${script_version}"
   print_property "script_dir" "${script_dir}"
   print_property "outdir" "${outdir}"
- 
+
   # Do validation and get needed properties from cluster
+  [[ -x "${script_dir}/yrs_flush_tables.sh" ]] || die "FATAL: ${script_dir}/yrs_flush_tables.sh does not exist or is not executable." 1
+
   do_connect -X -d yellowbrick
   rc=$?
   sleep 1
   [[ ${rc} -eq 0 ]] || die "FATAL: Failed to connect to Yellowbrick instance." 1
   print_property "connection" "success"
-  
+
   yb_ver_num="$(get_yb_ver_num -X )"
   [[ $? -eq 0 ]] || die "FATAL: Failed to get Yellowbrick server version number." 1
   print_property "yb_ver_num" "${yb_ver_num}"
 
   # Only CN and YB appliances > Ver 7 have default and system cluster
   if [[ ${yb_ver_num} -ge 70000 ]]; then
-  
+
     system_cluster="$(get_system_cluster -X)"
     [[ $? -eq 0 ]] || die "FATAL: Failed to get system_cluster." 1
     export YBCLUSTER="${system_cluster}"
-    
+
     default_cluster="$(get_default_cluster -X)"
     [[ $? -eq 0 ]] || die "FATAL: Failed to get default cluster." 1
-    
+
     is_cn="$(is_cn -X )"
     [[ $? -eq 0 ]] || die "FATAL: Failed to get is_cn." 1
   fi
-  
+
   print_property "system_cluster" "${system_cluster}"
-  print_property "default_cluster" "${default_cluster}"  
+  print_property "default_cluster" "${default_cluster}"
   print_property "is_cn" "${is_cn}"
-  
-  # Print the yrs reports for all cases.
-  print_section_hdr "yrs reports"
-  run_yrs_queries
-    
+
+  # Print the yrs reports unless -R|--no_report is specified.
+    if [[ -z ${opt_no_report} ]]; then
+      print_section_hdr "yrs reports"
+      run_yrs_queries
+    fi
+
   # Do the actual flushes
   if [[ -z ${opt_report_only} ]]; then
-
 
     print_section_hdr "Flush yrs user tables"
     ./yrs_flush_tables.sh "user" "${outdir}"
     print_section_hdr "Flush yrs sys tables"
     ./yrs_flush_tables.sh "sys"  "${outdir}"
-    print_section_hdr "Trim yrs files"
-    ${ybsql_cmd} -c "SELECT yb_yrs_delete_unused_files()"
 
-    print_section_hdr "yrs reports after yflush"
-    run_yrs_queries
+    if [[ -n ${opt_trim} ]]; then
+      print_section_hdr "Trim yrs files"
+      ${ybsql_cmd} -c "SELECT yb_yrs_delete_unused_files()"
+    fi
+
+    if [[ -z ${opt_no_report} ]]; then
+      print_section_hdr "yrs reports after yflush"
+      run_yrs_queries
+    fi
   fi
-  
+
   print_section_hdr "DONE"
   print_property "output_file"  "${outfile}"
   echo "${hr2}"
